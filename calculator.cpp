fff942db0cdc93f0fd04e95c3c49098548796d14
ashokoram <asashokoram@gmail.com>
2024-03-19 19:11:53 +0530

    add new changes

---
+++ calculator.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+  int num1, num2;
+  cout << "Enter two numbers: ";
+  cin >> num1 >> num2;
+ cout <<"the sum of two no:"<<num1+num2<<endl;
+ cout <<"the subtraction of two no:"<<num1-num2<<endl;
+ cout <<"the multiplication of two no:"<<num1*num2<<endl;
+ cout <<"the division of two no:"<<num1/num2<<endl;
+ cout <<"the modulo of two no:"<<num1%num2<<endl;
+ 
+  return 0;
+}
